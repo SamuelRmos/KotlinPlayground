8c262597678f90a38e712b1e66b1bc57a4c95173
Samuel Ramos <samuelramos.sjr@gmail.com>
2020-07-20 23:14:12 -0300

    basics conceptions

---
+++ app/src/main/java/com/example/kotlinplayground/basics/lambdas.kt
@@ -0,0 +1,20 @@
+package com.example.kotlinplayground.basics
+
+fun main(args: Array<String>) {
+    val list = (1..100).toList()
+
+    // println(list.filter({ element -> element % 2 == 0 }))
+
+    // println(list.filter { it % 2 == 0 })
+
+    println(list.filter { it.even() })
+
+    println(list.filter(::isEven))
+
+
+
+}
+
+fun isEven(i: Int) = i % 2 == 0
+
+fun Int.even() = this % 2 == 0
---
+++ app/src/main/java/com/example/kotlinplayground/basics/parameters.kt
@@ -0,0 +1,8 @@
+package com.example.kotlinplayground.basics
+
+fun main(args: Array<String>) {
+    val together = concat(listOf("Kotlin", "Java", "Scala"), " : ")
+    println(together)
+}
+
+fun concat(texts: List<String>, separator: String = ", ") = texts.joinToString(separator)
